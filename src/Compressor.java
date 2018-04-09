f5040744386a0707ecc3446d1a3a7a3a2a9982e3
XTech2K <xander@kimxtom.com>
2018-04-09 18:22:27 -0400

    Remove unused and broken PriorityQueue

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,9 +1,5 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.PriorityQueue;
 
 public class Compressor {
 
